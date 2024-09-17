e26ec7c67397d09c343a91600e7328f27e1e0ffe
Juan <juan2005ajedrez@gmail.com>
2024-09-16 23:28:46 -0500

    version 0.0.1 router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { Routes, Route } from "react-router-dom";
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import InicioSesion from './views/InicioSesion';
 import Perfil from './views/Perfil';
 import Calendario from './views/Calendario';
@@ -9,18 +8,18 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="app-container">
-      <Header />
-      <div className="app-content">
+    <Router>
+      <div>
+        <Header />
         <Routes>
           <Route path="/" element={<InicioSesion />} />
           <Route path="/perfil" element={<Perfil />} />
           <Route path="/calendario" element={<Calendario />} />
           <Route path="/inicio" element={<Inicio />} />
         </Routes>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
